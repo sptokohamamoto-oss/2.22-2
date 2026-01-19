ce0b9d13b53a8075581a76c6b5c247fcc33ce7d9
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-19 10:50:33 +0900

    a

--- ConsoleApp22.2/Program.cs
+++ ConsoleApp22.2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public interface IBillable
+{
+    int CostForDay(int hours);
+}
+
+public class FullTimeEmployee : IBillable
+{
+    private const int DailyRate = 20000;
+
+    public int CostForDay(int hours)
+    {
+        return DailyRate;
+    }
+}
+
+public class ContractEmployee : IBillable
+{
+    private const int HourlyRate = 3000;
+
+    public int CostForDay(int hours)
+    {
+        return hours * HourlyRate;
+    }
+}
+
+public class BillingCalculator
+{
+    public int CalculateCost(IBillable employee, int hours)
+    {
+        return employee.CostForDay(hours);
+    }
+}
